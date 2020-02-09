994ce02bc13bbbeb9114e59784efb9964361d7fd
matanelgabsi <matan.elgabsi@gmail.com>
2020-02-09 23:55:30 +0200

    Add progress bar
    This was changed as it's very difficult to add another JS splash screen that matches the android one..

--- android/src/main/java/com/zoontek/rnbootsplash/RNBootSplash.java
+++ android/src/main/java/com/zoontek/rnbootsplash/RNBootSplash.java
@@ -2,9 +2,12 @@ package com.zoontek.rnbootsplash;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
 import androidx.annotation.NonNull;
 
 public class RNBootSplash {
@@ -19,16 +22,18 @@ public class RNBootSplash {
       public void run() {
         Context context = activity.getApplicationContext();
         LinearLayout layout = new LinearLayout(context);
-        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-        View view = new View(context);
+        LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1);
+        params.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+        LayoutParams paramsParent = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        ProgressBar view = new ProgressBar(context);
 
-        view.setBackgroundResource(drawableResId);
+        layout.setBackgroundResource(drawableResId);
         layout.setId(R.id.bootsplash_layout_id);
         layout.setLayoutTransition(null);
-        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
         layout.addView(view, params);
 
-        activity.addContentView(layout, params);
+        activity.addContentView(layout, paramsParent);
         showHasRunOnce = true;
       }
     });
